493a4a91f1cc82a7c9a383e952910a14eb89bba7
deji2ghost <dejiwilliams9@gmail.com>
2026-05-02 22:35:01 +0100

    feat: uncommented interceptor.

--- lib/AxiosInstance/AxiosInstance.ts
+++ lib/AxiosInstance/AxiosInstance.ts
@@ -42,40 +42,40 @@ AxiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    // if (
-    //   error.response?.status === 401 &&
-    //   !originalRequest._retry &&
-    //   !originalRequest.url.includes("/auth/login") &&
-    //   !originalRequest.url.includes("/auth/signup") &&
-    //   !originalRequest.url.includes("/auth/verify-email")
-    // ) {
-    //   originalRequest._retry = true;
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/login") &&
+      !originalRequest.url.includes("/auth/signup") &&
+      !originalRequest.url.includes("/auth/verify-email")
+    ) {
+      originalRequest._retry = true;
 
-    //   try {
-    //     const refreshResponse = await AxiosInstance.post(
-    //       "/auth/refresh",
-    //       {},
-    //       { withCredentials: true },
-    //     );
+      try {
+        const refreshResponse = await AxiosInstance.post(
+          "/auth/refresh",
+          {},
+          { withCredentials: true },
+        );
 
-    //     const newAccessToken = refreshResponse.data?.access_token;
-    //     if (newAccessToken) {
-    //       tokenStore.set(newAccessToken);
-    //       // originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-    //       originalRequest.headers = {
-    //         ...(originalRequest.headers || {}),
-    //         Authorization: `Bearer ${newAccessToken}`,
-    //       };
-    //     }
-    //     return AxiosInstance(originalRequest);
-    //   } catch (refreshError) {
-    //     tokenStore.clear();
-    //     if (typeof window !== "undefined") {
-    //       window.location.href = "/sign-in";
-    //     }
-    //     return Promise.reject(refreshError);
-    //   }
-    // }
+        const newAccessToken = refreshResponse.data?.access_token;
+        if (newAccessToken) {
+          tokenStore.set(newAccessToken);
+          // originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          originalRequest.headers = {
+            ...(originalRequest.headers || {}),
+            Authorization: `Bearer ${newAccessToken}`,
+          };
+        }
+        return AxiosInstance(originalRequest);
+      } catch (refreshError) {
+        tokenStore.clear();
+        if (typeof window !== "undefined") {
+          window.location.href = "/sign-in";
+        }
+        return Promise.reject(refreshError);
+      }
+    }
     // NETWORK / TIMEOUT HANDLING
     let apiMessage = "Something went wrong";
     let status = error.response?.status || 500;
